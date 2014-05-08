41caa0b9df0c3cc4b13ba2b82d6346b6c7d31fa7
Jeremy Olliver <jeremy.olliver@gmail.com>
2014-05-08 18:20:56 +1200

    Comment out official debian support until GH#1 is merged

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      "Installs/Configures unattended-upgrades"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
-supports "debian"
+# supports "debian" # Untested
 supports "ubuntu"
 
 depends "apt"
