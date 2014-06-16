f279e5d487d6fc81aacc8209d5a2a6e7daf326bb
Stanislav Savulchik <s.savulchik@gmail.com>
2014-06-16 17:40:47 +0700

    Fix for #3

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,11 @@ maintainer_email "jeremy.olliver@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures unattended-upgrades"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 
 # supports "debian" # Untested
 supports "ubuntu"
 
 depends "apt"
 
-provides "recipe[unattended-upgrades::default]"
+provides "unattended-upgrades::default"
