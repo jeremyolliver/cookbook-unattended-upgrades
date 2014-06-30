2ecd58eb2fe2087d9adf2719e09d350a624758d5
Jeremy Olliver <jeremy.olliver@gmail.com>
2014-07-01 11:56:21 +1200

    Version bump to 0.1.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "jeremy.olliver@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures unattended-upgrades"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
+version          "0.1.2"
 
 # supports "debian" # Untested
 supports "ubuntu"
